8b5305468123740cb403175ad180daf11fcd54bf
Ruwaiz <ruwaizrazak@gmail.com>
2020-10-20 17:27:03 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -10,10 +10,3 @@ export default function(Vue, { head }) {
   };
 }
 
-export default function (Vue, { head }) {
-  head.link.push({
-    rel: 'stylesheet',
-    href: 'https://fonts.googleapis.com/css2?family=Roboto'
-  })
-}
-
